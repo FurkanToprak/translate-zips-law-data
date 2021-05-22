080aec26a0d933c8e7f9c18c0e2d338cdaf697c8
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-05-22 17:10:00 -0500

    minimal working version./deployScript.sh

--- translate.py
+++ translate.py
@@ -1,13 +1,34 @@
 from os import listdir
+from googletrans import Translator
 
-dir_files = listdir(".")
+# translator object
+translator = Translator()
 
-
+# scrape file names & supported languages
 def is_lang_file(file_name):
     return file_name.split("-")[0] == 'word'
 
-
-language_files = filter(is_lang_file, dir_files)
+dir_files = listdir(".")
+language_files = list(filter(is_lang_file, dir_files))
+languages = list(
+    map(lambda language_file: language_file.split('-')[2], language_files))
 
 for language_file in language_files:
-    print(language_file)
+    # open and read file
+    lang_file = open(language_file, 'r')
+    read_lang_file = lang_file.read()
+    words = read_lang_file.split('\n')
+    # get top 100 or however many there exist
+    top_100_words = list(map(lambda line: line.split(',')[
+                         0], words[1:min(101, len(words))]))
+    this_language = language_file.split('-')[2]
+    other_languages = filter(
+        lambda language: language != this_language, languages)
+    for language in other_languages:
+        res = translator.translate(
+            top_100_words, src=this_language, dest=language)
+        print(res)
+        print('done')
+        exit()
+
+    lang_file.close()
