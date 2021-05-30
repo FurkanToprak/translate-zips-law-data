ecaa9385f16c03cde98dfc1ba92b9d736f3d51fc
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-05-30 18:40:47 -0500

    connection string.

--- upload.py
+++ upload.py
@@ -3,8 +3,12 @@ from os import listdir, getenv
 from dotenv import load_dotenv
 
 load_dotenv()
+
 MONGO_USERNAME = getenv('MONGO_USERNAME')
 MONGO_PASSWORD = getenv('MONGO_PASSWORD')
-client_connection =  f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@<cluster-url>/test?retryWrites=true&w=majority"
+MONGO_CLUSTER_URL = getenv('MONGO_CLUSTER_URL')
+MONGO_DATABASE = getenv('MONGO_DATABASE')
+
+client_connection =  f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_CLUSTER_URL}/{MONGO_DATABASE}?retryWrites=true&w=majority"
 client = pymongo.MongoClient(client_connection)
 db = client.test
