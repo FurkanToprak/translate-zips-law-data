80fd8a93af0e469addacec05ccf05ab3916325c1
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-05-30 23:44:09 -0500

    populates database correctly.

--- upload.py
+++ upload.py
@@ -7,9 +7,16 @@ import re
 # captures the alpha2 code of the word-freq file
 freq_regex = re.compile('^word-freq-(.+)-(.+).csv')
 
+
 class Schema:
     def document(self):
         raise NotImplementedError("Implement document getter.")
+
+    def set_id(self, id):
+        self.id = id
+
+    def get_id(self):
+        return self.id
 
 
 class Language(Schema):
@@ -26,6 +33,7 @@ class Language(Schema):
             "name": self.name
         }
 
+
 class RankedWord(Schema):
     def __init__(self, rank, translation_id):
         self.rank = rank
@@ -37,26 +45,29 @@ class RankedWord(Schema):
             "translation": self.translation_id
         }
 
+
 class Translation(Schema):
     def __init__(self):
         self.trans_map = dict()
-    
+
     def put(self, language, word):
         self.trans_map[language] = word
 
     def document(self):
         return self.trans_map
 
+
 class KnowledgeBase(Schema):
-    def __init__(self, language, words):
-        self.language = language
-        self.words = words
+    def __init__(self, language_id, words_id):
+        self.language = language_id
+        self.words = words_id
 
     def document(self):
         return {
             "language": self.language,
             "words": self.words
         }
+
 
 load_dotenv()
 
@@ -72,12 +83,13 @@ db = client.alpha
 language_collection = db.languages
 translation_collection = db.translations
 ranked_words_collection = db.ranked_words
-
+knowledge_base_collection = db.knowledge_bases
 # Populate language collection
 for language in languages:
     if len(language.alpha2) > 0:
         language_document = Language(language.alpha2, language.name)
-        # language_collection.insert_one(language_document.document())
+        language_id = language_collection.insert_one(language_document.document()).inserted_id
+        language_document.set_id(language_id)
 
 
 # Populate translations collection
@@ -103,21 +115,35 @@ for directory_file in directory_files:
         translation_lines = translation_file.read().split('\n')
         translation_file.close()
         translation_grid = []
-        num_rows = len(translation_lines[1].split(',')) # num of ranks there are
-        num_cols = len(translation_lines) - 1 # num of languages
+        # num of ranks there are
+        num_rows = len(translation_lines[1].split(','))
+        num_cols = len(translation_lines) - 1  # num of languages
         print(translation_file_name, 'cols:', num_cols, 'rows:', num_rows)
         trans_objs = []
+        ranked_word_ids = []
+        # TODO: insert_one should be insert_many
         for row in range(num_rows):
             rank = 1 + row
             translation_row = []
             trans_obj = Translation()
             for col in range(1, num_cols):
                 lang = translation_lines[0].split(',')[col - 1]
-                translation = translation_lines[col].split(',')[row]
+                try:
+                    translation = translation_lines[col].split(',')[row]
+                except:
+                    break
                 trans_obj.put(lang, translation)
             trans_obj.put(alpha2_code, ranked_words[row])
-            translation_id = translation_collection.insert_one(trans_obj.document()).inserted_id
+            translation_id = translation_collection.insert_one(
+                trans_obj.document()).inserted_id
+            trans_obj.set_id(translation_id)
             ranked_word = RankedWord(rank, translation_id)
-            ranked_word_id = ranked_words_collection.insert_one(ranked_word.document()).inserted_id
-
-        exit()
+            ranked_word_id = ranked_words_collection.insert_one(
+                ranked_word.document()).inserted_id
+            ranked_word.set_id(ranked_word_id)
+            ranked_word_ids.append(ranked_word_id)
+        # print(ranked_word_ids)
+        language_id = language_collection.find_one({'alpha2': alpha2_code})
+        # print(language_id)
+        knowledge_base = KnowledgeBase(language_id, ranked_word_ids)
+        knowledge_base_collection.insert_one(knowledge_base.document())
